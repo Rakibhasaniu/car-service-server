636a1cdde6f05c834a5352d79b14482c69a8d9e3
toukircse77 <toukircse777@gmail.com>
2022-11-29 17:05:09 +0600

    post advertice data

--- index.js
+++ index.js
@@ -105,11 +105,7 @@ async function run() {
         //Advertise Car Modal information
 
 
-        app.post('/advertic', async (req, res) => {
-            const boking = req.body;
-            const result = await adverticCollection.insertOne(boking);
-            res.send(result);
-        })
+       
 
 
             //Booking receive on email user 
